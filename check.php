dcd2cb416c376c511c3370d3780242035e093bbe
Napont Kitiwiriyakul <mrnonaki@gmail.com>
2018-10-17 12:58:51 +0700

    cases nbtc server issues

--- check.php
+++ check.php
@@ -15,26 +15,31 @@ $file = fopen($db.$callsign, 'w');
 $txt = file_get_contents("http://apps.nbtc.go.th/callsign/result.php?search=$callsign&ยืนยัน=ยืนยัน");
 $pic = file_get_contents("https://www.qrz.com/db/$callsign");
 
-if (strpos($txt, "ไม่พบข้อมูลที่ต้องการค้นหา") !== false) {
-	fwrite($file, "error");
+if ($txt) {
+	if (strpos($txt, "ไม่พบข้อมูลที่ต้องการค้นหา") !== false) {
+		fwrite($file, "error");
+	} elseif (strpos($txt, "Server มีปัญหากรุณารอซักครู่เพื่อเข้าสู่เว็บไซต์") !== false){
+		exit(nbtc-server-issues);
+	} else {
+		$postxt = strpos($txt, '<td><div align="center">1</div></td>');
+		$txt = substr($txt, $postxt);
+		$postxt = strpos($txt, '</div></td></tr>');
+		$txt = substr($txt, 0, $postxt);
+		$arraytxt = (explode(' ',$txt));
+
+		$pospic = strpos($pic, 'ppic');
+		$pic = substr($pic, $pospic);
+		$pospic = strpos($pic, 'mypic');
+		$pic = substr($pic, 0, $pospic);
+		$arraypic = (explode(' ',$pic));
+		$pic = substr($arraypic[10], 5, -1);
+
+		$ltxt = $arraytxt[97] . " " . $arraytxt[98] . " " . $arraytxt[112] ." " . $arraytxt[182] . " " . $arraytxt[20] . " " . strtotime($arraytxt[124] . $arraytxt[125] . $arraytxt[126]) . " " . strtotime($arraytxt[141] . $arraytxt[142] . $arraytxt[143]) . " " . $pic;
+		fwrite($file, $ltxt);
+		}
 } else {
-	$postxt = strpos($txt, '<td><div align="center">1</div></td>');
-	$txt = substr($txt, $postxt);
-	$postxt = strpos($txt, '</div></td></tr>');
-	$txt = substr($txt, 0, $postxt);
-	$arraytxt = (explode(' ',$txt));
-
-	$pospic = strpos($pic, 'ppic');
-	$pic = substr($pic, $pospic);
-	$pospic = strpos($pic, 'mypic');
-	$pic = substr($pic, 0, $pospic);
-	$arraypic = (explode(' ',$pic));
-	$pic = substr($arraypic[10], 5, -1);
-
-	$ltxt = $arraytxt[97] . " " . $arraytxt[98] . " " . $arraytxt[112] ." " . $arraytxt[182] . " " . $arraytxt[20] . " " . strtotime($arraytxt[124] . $arraytxt[125] . $arraytxt[126]) . " " . strtotime($arraytxt[141] . $arraytxt[142] . $arraytxt[143]) . " " . $pic;
-	fwrite($file, $ltxt);
-	}
-	
-	fclose($file);
-	exec('sudo mount -o remount,ro /');
+	exit(nbtc-server-down);
+}
+fclose($file);
+exec('sudo mount -o remount,ro /');
 ?>
